295170208827687de945a171f8db68ab1d83b291
Joel Borchardt <joelborchardt@Joels-MacBook-Pro.local>
2018-10-03 11:06:45 +0400

    added hide/unhide for links

--- imports/api/links.js
+++ imports/api/links.js
@@ -29,7 +29,31 @@ Meteor.methods({
         Links.insert({
             _id: shortid.generate(),
             url,
+            userID: this.userId,
+            visible: true
+        });
+    },
+    'links.setVisibility'(_id, visible) {
+        if (!this.userId){
+            throw new Meteor.Error('not-authorized');
+        }
+
+        new SimpleSchema({
+            _id: {
+                type: String,
+                min: 1
+            },
+            visible: {
+                type: Boolean,
+            }
+        }).validate({ _id, visible });
+
+        Links.update({
+            _id,
             userID: this.userId
+        }, {
+            $set: { visible }
         });
     }
+
 });
--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Clipboard from 'clipboard';
+import { Meteor } from 'meteor/meteor';
 
 
 export default class LinksListItem extends React.Component {
@@ -30,8 +31,14 @@ export default class LinksListItem extends React.Component {
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
+                <p>{this.props.visible.toString()}</p>
                 <button ref="copy" data-clipboard-text={this.props.shortUrl}>
                     {this.state.justCopied ? 'Copied' : 'Copy'}
+                </button>
+                <button onClick={() => {
+                    Meteor.call('links.setVisibility', this.props._id, !this.props.visible)
+                }}>
+                    {this.props.visible ? 'Hide' : 'Unhide'}
                 </button>
             </div>
         );
